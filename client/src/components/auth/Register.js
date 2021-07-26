02a817e467d32daad4cdb49caa9c4e1d74b5eb96
Maurice <kollewem_35@yahoo.com>
2021-07-26 08:50:49 +0100

    update register page

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,12 +1,46 @@
 import React from 'react'
+import Form from 'react-bootstrap/Form'
 
 
 const Register = () => {
 
   return (
-    <>
-      <h1>Register is here</h1>
-    </>
+    <Form>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>User name</Form.Label>
+        <Form.Control type="username" placeholder="Enter username" />
+        <Form.Text className="text-muted">
+          I will never share your email with anyone else.
+        </Form.Text>
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Email</Form.Label>
+        <Form.Control type="email" placeholder="Email" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" placeholder="Password" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Password confirmation</Form.Label>
+        <Form.Control type="passwordConfirmation" placeholder="Password confirmation" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>First name</Form.Label>
+        <Form.Control type="firstName" placeholder="First name" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Last name</Form.Label>
+        <Form.Control type="lastName" placeholder="Last name" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicCheckbox">
+        <Form.Check type="checkbox" label="Check me out" />
+      </Form.Group>
+      <button variant="primary" type="submit">
+        Submit
+      </button>
+    </Form>
   )
 }
 
