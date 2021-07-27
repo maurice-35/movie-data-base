4ba02662a29b5e8959cab208b6860aa8900842cc
Maurice <kollewem_35@yahoo.com>
2021-07-27 17:35:58 +0100

    update register page

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -11,28 +11,34 @@ const Register = () => {
     password_confirmation: '',
     first_name: '',
     last_name: '',
-    profile_image: '',    
+    profile_image: '',
   })
 
-  const handleChange = () => {
-    console.log('changing')
+  const handleChange = (event) => {
+    const newFormdata = { ...formdata, [event.target.name]: event.target.value }
+    setFormdata(newFormdata)
   }
-    
 
+  const handleSubmit = (event) => {
+  event.preventDefault()
+    console.log('Submitted')
+  }
+
+  console.log('formdata on state', formdata)
   return (
     <section className="section">
       <div className="container">
         <div className="columns">
-          <form className="column is-half is-offset-one-quarter box">
+          <form onSubmit={handleSubmit} className="column is-half is-offset-one-quarter box">
             <div className="field">
               <label className="label">Username</label>
               <div className="control">
                 <input
-                // className={`input ${errors.username ? 'is-danger' : ''}`}
-                placeholder="Username"
-                onChange={handleChange}
-                name="username"
-                value={formdata.username}
+                  // className={`input ${errors.username ? 'is-danger' : ''}`}
+                  placeholder="Username"
+                  onChange={handleChange}
+                  name="username"
+                  value={formdata.username}
                 />
               </div>
               {/* {errors.username && <p className="help is-danger">{errors.username}</p>} */}
@@ -42,11 +48,11 @@ const Register = () => {
               <div className="control">
                 <input
 
-                // className={`input ${errors.email ? 'is-danger' : ''}`}
-                placeholder="Email"
-                onChange={handleChange}
-                name="email"
-                value={formdata.email}
+                  // className={`input ${errors.email ? 'is-danger' : ''}`}
+                  placeholder="Email"
+                  onChange={handleChange}
+                  name="email"
+                  value={formdata.email}
                 />
               </div>
               {/* {errors.email && <p className="help is-danger">{errors.email}</p>} */}
@@ -55,12 +61,12 @@ const Register = () => {
               <label className="label">Password</label>
               <div className="control">
                 <input
-                type="password"
-                // className={`input ${errors.password ? 'is-danger' : ''}`}
-                placeholder="Password"
-                onChange={handleChange}
-                name="password"
-                value={formdata.password}
+                  type="password"
+                  // className={`input ${errors.password ? 'is-danger' : ''}`}
+                  placeholder="Password"
+                  onChange={handleChange}
+                  name="password"
+                  value={formdata.password}
                 />
               </div>
               {/* {errors.password && <p className="help is-danger">{errors.password}</p>} */}
@@ -69,12 +75,12 @@ const Register = () => {
               <label className="label">Password Confirmation</label>
               <div className="control">
                 <input
-                // type="password"
-                // className={`input ${errors.passwordConfirmation ? 'is-danger' : ''}`}
-                // placeholder="Password Confirmation"
-                // onChange={handleChange}
-                // name="passwordConfirmation"
-                value={formdata.passwordConfirmation}
+                  type="password"
+                  // className={`input ${errors.passwordConfirmation ? 'is-danger' : ''}`}
+                  placeholder="Password Confirmation"
+                  onChange={handleChange}
+                  name="passwordConfirmation"
+                  value={formdata.passwordConfirmation}
                 />
               </div>
               {/* {errors.passwordConfirmation && <p className="help is-danger">{errors.passwordConfirmation}</p>} */}
@@ -83,28 +89,35 @@ const Register = () => {
               <label className="label">First name</label>
               <div className="control">
                 <input
-                value={formdata.first_name}
+                  placeholder="First Name"
+                  onChange={handleChange}
+                  name="first_name"
+                  value={formdata.first_name}
                 />
-                </div>
-            <div className="field">
-              <label className="label">Last name</label>
-              <div className="control">
-                <input
-                value={formdata.last_name}
-                />
+              </div>
+              <div className="field">
+                <label className="label">Last name</label>
+                <div className="control">
+                  <input
+                    placeholder="Last Name"
+                    onChange={handleChange}
+                    name="last_name"
+                    value={formdata.last_name}
+                  />
                 </div>
                 <div className="field">
-              <label className="label">Profile image</label>
-              <div className="control">
-                <input
-                value={formdata.profile_image}
-                />
+                  <label className="label">Profile image</label>
+                  <div className="control">
+                    <input
+                      placeholder="Upload profile image"
+                      value={formdata.profile_image}
+                    />
+                  </div>
+                  <div className="field">
+                    <button type="submit" className="button is-fullwidth is-warning">Register Me!</button>
+                  </div>
                 </div>
-            <div className="field">
-              <button type="submit" className="button is-fullwidth is-warning">Register Me!</button>
-            </div>
-            </div>
-            </div>
+              </div>
             </div>
           </form>
         </div>
